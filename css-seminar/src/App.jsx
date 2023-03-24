a6cb8495736d8bbdd939db1f141e13d7578c4b14
August Aldred <hachigatsu14@outlook.com>
2023-03-24 08:44:23 +0000

    first commit

---
+++ css-seminar/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <main>
+        <h1>Practicing CSS</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
